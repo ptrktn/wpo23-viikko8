2fce0957368eba436bfa2063f2ecedc7da05a2ae
Petteri Kettunen <ptrktn@users.noreply.github.com>
2023-10-19 20:53:06 +0900

    Add exercise 8

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -7,6 +7,7 @@ class RatingsController < ApplicationController
   
   def new
     @rating = Rating.new
+    @beers = Beer.all
   end
 
   def create
--- app/models/beer.rb
+++ app/models/beer.rb
@@ -5,4 +5,8 @@ class Beer < ApplicationRecord
   def average_rating
     self.ratings.average(:score).to_f
   end
+
+  def to_s
+    "#{self.brewery.name} #{self.name}"
+  end
 end
