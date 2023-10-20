31e9ce20ca1fb13a52d530cd943c6488ee5ff18c
Petteri Kettunen <ptrktn@users.noreply.github.com>
2023-10-21 06:40:51 +0900

    Add exercise 14

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,5 +1,10 @@
 class Brewery < ApplicationRecord
   has_many :beers, dependent: :destroy
+  has_many :ratings, through: :beers
+
+  def average_rating
+    self.ratings.average(:score).to_f
+  end
 
   def to_s
     "#{self.name}"
