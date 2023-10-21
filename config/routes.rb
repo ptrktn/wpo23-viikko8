6c0484d214c366fdf1828435221b85ce1630ec01
Petteri Kettunen <ptrktn@users.noreply.github.com>
2023-10-21 09:47:28 +0900

    Add exercise 4

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,10 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root 'breweries#index'
 
-  # get 'ratings', to: 'ratings#index'
-  # get 'ratings/new', to:'ratings#new'
-  # post 'ratings', to: 'ratings#create'
   resources :ratings, only: [:index, :new, :create, :destroy]
   resource :session, only: [:new, :create, :destroy]
 
   get 'signup', to: 'users#new'
+  get 'signin', to: 'sessions#new'
+  delete 'signout', to: 'sessions#destroy'
 end
