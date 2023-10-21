19fcc5dae5a392b1ee3545ae13a4f5580647ac63
Petteri Kettunen <ptrktn@users.noreply.github.com>
2023-10-21 14:52:42 +0900

    Add exercise 8

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -5,9 +5,18 @@ class Brewery < ApplicationRecord
   has_many :ratings, through: :beers
 
   validates :name, presence: true
-  validates :year, inclusion: { in: 1040..2022 }
+  validates :year, presence: true,
+                   numericality: {
+                     only_integer: true,
+                     greater_than_or_equal_to: 1040
+                   }
+  validate :year_not_in_future
 
   def to_s
     name.to_s
+  end
+
+  def year_not_in_future
+    errors.add(:year, "brewery year is in future") if year > Date.current.year
   end
 end
