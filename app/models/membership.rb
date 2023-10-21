124a50c24613f0da30928dbb8876dc3833552141
Petteri Kettunen <ptrktn@users.noreply.github.com>
2023-10-21 16:48:43 +0900

    Add exercise 11

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,4 +1,9 @@
 class Membership < ApplicationRecord
   belongs_to :beer_club
   belongs_to :user
+
+  validates :beer_club_id, uniqueness: {
+    scope: :user_id,
+    message: "User can join a given club only once"
+  }
 end
