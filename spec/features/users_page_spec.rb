7c91173ea7a56102997a6427ccfa9a29465a2e9a
Petteri Kettunen <ptrktn@users.noreply.github.com>
2023-10-23 22:05:05 +0900

    Add exercise 9

--- spec/features/users_page_spec.rb
+++ spec/features/users_page_spec.rb
@@ -7,6 +7,18 @@ describe "User" do
     @user = FactoryBot.create :user
   end
 
+  it "when signed up with good credentials, is added to the system" do
+    visit signup_path
+
+    fill_in('user_username', with: 'Brian')
+    fill_in('user_password', with: 'Secret55')
+    fill_in('user_password_confirmation', with: 'Secret55')
+
+    expect {
+      click_button('Create User')
+    }.to change{ User.count }.by(1)
+  end
+
   describe "who has signed up" do
     it "can sign in with right credentials" do
       sign_in(username: "Pekka", password: "Foobar1")
@@ -26,16 +38,6 @@ describe "User" do
       expect(page).to have_content 'Username and/or password mismatch'
     end
 
-    it "shows user's own ratings" do
-      beer = create_beer_with_rating({ user: @user }, 10)
-      beer2 = create_beer_with_rating({ user: FactoryBot.create(:user, username: 'Vilho') }, 20)
-
-      visit user_path(@user)
-
-      expect(page).to have_content "#{beer.name} 10"
-      expect(page).not_to have_content "#{beer2.name} 20"
-    end
-
     it "removes ratings from database when user clicks delete" do
       create_beer_with_rating({ user: @user }, 10)
       create_beer_with_rating({ user: @user }, 10)
@@ -50,17 +52,31 @@ describe "User" do
         }.to change{ Rating.count }.by(-1)
       end
     end
-  end
 
-  it "when signed up with good credentials, is added to the system" do
-    visit signup_path
+    it "shows user's ratings" do
+      beer = create_beer_with_rating({ user: @user }, 10)
+      beer2 = create_beer_with_rating({ user: FactoryBot.create(:user, username: 'Vilho') }, 20)
 
-    fill_in('user_username', with: 'Brian')
-    fill_in('user_password', with: 'Secret55')
-    fill_in('user_password_confirmation', with: 'Secret55')
+      visit user_path(@user)
 
-    expect {
-      click_button('Create User')
-    }.to change{ User.count }.by(1)
+      expect(page).to have_content "#{beer.name} 10"
+      expect(page).not_to have_content "#{beer2.name} 20"
+    end
+
+    it "shows user's favorite beer style" do
+      create_beer_with_rating({ user: @user, style: 'Watery' }, 10)
+
+      visit user_path(@user)
+
+      expect(page).to have_content "User's favorite style is Watery"
+    end
+
+    it "shows user's favorite brewery" do
+      create_beer_with_rating({ user: @user, brewery: FactoryBot.create(:brewery, name: 'Pebbles') }, 10)
+
+      visit user_path(@user)
+
+      expect(page).to have_content "User's favorite brewery is Pebbles"
+    end
   end
 end
