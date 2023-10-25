d4821f1f0a526a6fb1fbc966710ad0e7f7d240bb
Petteri Kettunen <ptrktn@users.noreply.github.com>
2023-10-25 20:41:27 +0900

    Add week 5 exercise 4

--- lib/beermapping_api.rb
+++ lib/beermapping_api.rb
@@ -1,5 +1,10 @@
 class BeermappingApi
   def self.places_in(city)
+    city = city.downcase
+    Rails.cache.fetch(city, expires_in: 7.days) { get_places_in(city) }
+  end
+
+  def self.get_places_in(city)
     url = "http://beermapping.com/webservice/loccity/#{key}/"
 
     response = HTTParty.get "#{url}#{ERB::Util.url_encode(city)}"
