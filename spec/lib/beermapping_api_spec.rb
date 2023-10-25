cb381852fb4ab94a6a2eea9be5fa75737d08f96b
Petteri Kettunen <ptrktn@users.noreply.github.com>
2023-10-25 21:03:26 +0900

    Fix race condition due to using the same cache key in unit tests

--- spec/lib/beermapping_api_spec.rb
+++ spec/lib/beermapping_api_spec.rb
@@ -6,14 +6,14 @@ describe "BeermappingApi" do
       <?xml version='1.0' encoding='utf-8' ?><bmp_locations><location><id></id><name></name><status></status><reviewlink></reviewlink><proxylink></proxylink><blogmap></blogmap><street></street><city></city><state></state><zip></zip><country></country><phone></phone><overall></overall><imagecount></imagecount></location></bmp_locations>
     END_OF_STRING
 
-    stub_request(:get, /.*turku/).to_return(body: canned_answer, headers: { 'Content-Type' => "text/xml" })
+    stub_request(:get, /.*pori/).to_return(body: canned_answer, headers: { 'Content-Type' => "text/xml" })
 
-    places = BeermappingApi.places_in("turku")
+    places = BeermappingApi.places_in("pori")
 
     expect(places).to eq([])
   end
 
-  it "When HTTP GET returns one entry, it is parsed and returned" do
+  it "when HTTP GET returns one entry, it is parsed and returned" do
     canned_answer = <<~END_OF_STRING
       <?xml version='1.0' encoding='utf-8' ?><bmp_locations><location><id>18856</id><name>Panimoravintola Koulu</name><status>Brewpub</status><reviewlink>https://beermapping.com/location/18856</reviewlink><proxylink>http://beermapping.com/maps/proxymaps.php?locid=18856&amp;d=5</proxylink><blogmap>http://beermapping.com/maps/blogproxy.php?locid=18856&amp;d=1&amp;type=norm</blogmap><street>Eerikinkatu 18</street><city>Turku</city><state></state><zip>20100</zip><country>Finland</country><phone>(02) 274 5757</phone><overall>0</overall><imagecount>0</imagecount></location></bmp_locations>
     END_OF_STRING
@@ -33,9 +33,9 @@ describe "BeermappingApi" do
       <?xml version='1.0' encoding='utf-8' ?><bmp_locations><location><id>6742</id><name>Pullman Bar</name><status>Beer Bar</status><reviewlink>https://beermapping.com/location/6742</reviewlink><proxylink>http://beermapping.com/maps/proxymaps.php?locid=6742&amp;d=5</proxylink><blogmap>http://beermapping.com/maps/blogproxy.php?locid=6742&amp;d=1&amp;type=norm</blogmap><street>Kaivokatu 1</street><city>Helsinki</city><state></state><zip>00100</zip><country>Finland</country><phone>+358 9 0307 22</phone><overall>72.500025</overall><imagecount>0</imagecount></location><location><id>6743</id><name>Belge</name><status>Beer Bar</status><reviewlink>https://beermapping.com/location/6743</reviewlink><proxylink>http://beermapping.com/maps/proxymaps.php?locid=6743&amp;d=5</proxylink><blogmap>http://beermapping.com/maps/blogproxy.php?locid=6743&amp;d=1&amp;type=norm</blogmap><street>Kluuvikatu 5</street><city>Helsinki</city><state></state><zip>00100</zip><country>Finland</country><phone>+358 10 766 35</phone><overall>67.499925</overall><imagecount>1</imagecount></location></bmp_locations>
     END_OF_STRING
 
-    stub_request(:get, /.*turku/).to_return(body: canned_answer, headers: { 'Content-Type' => "text/xml" })
+    stub_request(:get, /.*helsinki/).to_return(body: canned_answer, headers: { 'Content-Type' => "text/xml" })
 
-    places = BeermappingApi.places_in("turku")
+    places = BeermappingApi.places_in("helsinki")
 
     expect(places.size).to eq(2)
 
