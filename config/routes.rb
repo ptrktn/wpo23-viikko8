a21a2b9c4666863f7ad0599eb7868ea67278dbb0
Petteri Kettunen <ptrktn@users.noreply.github.com>
2023-10-26 20:33:34 +0900

    Add week 5 exercises 5-6

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   get 'signin', to: 'sessions#new'
   delete 'signout', to: 'sessions#destroy'
-  get 'places', to: 'places#index'
+
+  resources :places, only: [:index, :show]
   post 'places', to: 'places#search'
 end
--- lib/beermapping_api.rb
+++ lib/beermapping_api.rb
@@ -1,7 +1,17 @@
 class BeermappingApi
   def self.places_in(city)
     city = city.downcase
-    Rails.cache.fetch(city, expires_in: 7.days) { get_places_in(city) }
+    places = Rails.cache.fetch(city, expires_in: 7.days) { get_places_in(city) }
+
+    places.each do |place|
+      Rails.cache.write(place.id.to_s, place)
+    end
+
+    places
+  end
+
+  def self.place(bm_id)
+    Rails.cache.fetch(bm_id, expires_in: 7.days) { get_place(bm_id) }
   end
 
   def self.get_places_in(city)
@@ -18,7 +28,20 @@ class BeermappingApi
     end
   end
 
+  def self.get_place(bm_id)
+    response = HTTParty.get "http://beermapping.com/webservice/locquery/#{key}/#{bm_id}"
+    place = response.parsed_response["bmp_locations"]["location"]
+
+    return nil if place.is_a?(Hash) && place['id'].nil?
+
+    Place.new(place)
+  end
+
   def self.key
-    ENV.fetch('BEERMAPPING_API_KEY', nil)
+    return nil if Rails.env.test?
+
+    raise 'BEERMAPPING_API_KEY env variable not defined' if ENV['BEERMAPPING_API_KEY'].nil?
+
+    ENV.fetch('BEERMAPPING_API_KEY')
   end
 end
