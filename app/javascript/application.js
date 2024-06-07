d6df59446122b61a51bd46416239d153f2e950f5
Petteri Kettunen <ptrktn@users.noreply.github.com>
2024-06-07 20:23:27 +0900

    Add week 8 exercise 7

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -10,3 +10,23 @@ import { beers, breweries } from "custom/utils";
 
 beers();
 breweries();
+
+// https://www.youtube.com/watch?v=jn22KyMEdwg
+// https://www.beflagrant.com/blog/turbo-confirmation-bias-2024-01-10
+// https://github.com/gorails-screencasts/custom-turbo-confirm-modal/blob/master/app/javascript/application.js
+Turbo.setConfirmMethod((message, element, submitter) => {
+	let dialog = document.getElementById("turbo-confirm-custom");
+	let [messageText, confirmText, cancelText] = message.split(';');
+	dialog.querySelector("#message").innerHTML = messageText;
+	dialog.querySelector("#confirm").textContent =
+		submitter?.dataset.confirmButton || confirmText || 'Yes' ;
+	dialog.querySelector("#cancel").textContent =
+		submitter?.dataset.cancelButton || cancelText || 'No' ;
+	dialog.showModal();
+
+	return new Promise((resolve, reject) => {
+		dialog.addEventListener("close", () => {
+			resolve(dialog.returnValue == "confirm")
+		}, { once: true })
+	});
+});
